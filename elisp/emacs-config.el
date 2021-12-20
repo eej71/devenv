08883b316a4bdf5e15e82a0b31b2c6065ad73ef8
Eric Johnson <johnson.eric@gmail.com>
2021-12-20 06:10:32 -0500

    Removed some debris

--- elisp/emacs-config.el
+++ elisp/emacs-config.el
@@ -54,10 +54,7 @@
 (global-set-key [f2] 'next-error)
 (define-key company-active-map (kbd "C-n") 'company-select-next)
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
-
 (menu-bar-mode -1)
-
-
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 (provide 'emacs-config)
