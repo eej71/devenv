a34d3ead5b8e0944ef7a966705e4a3f2973a025d
Eric Johnson <johnson.eric@gmail.com>
2021-12-20 06:00:27 -0500

    Removed pointless comments

--- elisp/prelude-modules.el
+++ elisp/prelude-modules.el
@@ -1,13 +1,10 @@
 ;;; Uncomment the modules you'd like to use and restart Prelude afterwards
 
-;; Emacs IRC client
 (require 'prelude-company)
 (require 'prelude-selectrum)
-
-;;; Programming languages support
 (require 'prelude-c)
 (require 'prelude-emacs-lisp)
-(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
+(require 'prelude-org)
 (require 'prelude-perl)
 (require 'prelude-lsp)
 (require 'prelude-python)
