2adc0277abdef3db94d43979b36bb5adcea36b21
Eric Johnson <johnson.eric@gmail.com>
2026-03-02 14:57:49 -0500

    Disable xterm-mouse-mode so i can get back my simple selection to clipboard

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -91,6 +91,11 @@
 (put 'narrow-to-region 'disabled nil)
 (put 'compilation-search-path 'safe-local-variable 'string-or-null-p)
 
+;; Don't grab mouse events in terminal Emacs (keeps Windows Terminal selection working)
+;; TODO: This should be put into something more generic for frame creation
+(unless (display-graphic-p)
+  (xterm-mouse-mode -1))
+
 (use-package ack :init
   (setq
    ack-and-a-half-arguments "--ignore-dir=release --ignore-dir=debug"))
